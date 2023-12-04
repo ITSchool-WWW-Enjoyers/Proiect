b95702c0ff02909819813d652dbc446b5888dd37
ChrisTheSpud <cristian.bendea19@gmail.com>
2023-12-04 18:36:23 +0200

    Created a Canvas and Toolbar component

---
+++ src/Components/Toolbar.jsx
@@ -0,0 +1,137 @@
+import { DrawingContext } from '../Contexts/DrawingContext';
+import undosvg from '../assets/undo.svg';
+import redosvg from '../assets/redo.svg';
+import resetsvg from '../assets/reset.svg';
+import pentoolsvg from '../assets/pentool.svg';
+import erasersvg from '../assets/eraser.svg';
+import square from '../assets/squareshape.svg';
+import ellipse from '../assets/ellipseshape.svg';
+import triangle from '../assets/triangleshape.svg';
+import filltool from '../assets/filltool.svg';
+import './GlobalButtons.scss';
+import { useContext } from 'react';
+
+function Toolbar() {
+
+    const { undo, redo, resetCanvas, strokeSize , eraser, colorPicker, fillCanvas } = useContext(DrawingContext);
+
+    return (
+        <>
+        <div className='toolbar'>
+            <button
+            className='btn-decoration'
+            onClick={undo}
+            >
+                <img src={undosvg} alt="Undo button"></img>
+            </button>
+            <button
+            className='btn-decoration'
+            onClick={redo}
+            >
+            <img src={redosvg} alt="Redo button"></img>
+            </button>
+            <button
+                className='btn-decoration'
+                onClick={resetCanvas}
+            >
+                <img src={resetsvg} alt="Reset button"></img>
+            </button>
+            <label className="label-group">
+                <input 
+                    type="radio" 
+                    name="drawingtool" 
+                    className="drawing-input"
+                    id="pen"
+                    defaultChecked
+                >
+                </input>
+                <img 
+                    className="draw-image" 
+                    src={pentoolsvg}
+                    alt="Pen tool"
+                >
+                </img>
+            </label>
+            <label className="label-group">
+                <input 
+                    type="radio" 
+                    name="drawingtool" 
+                    className="drawing-input"
+                    id="eraser"
+                    onClick={eraser}
+                >
+                </input>
+                <img 
+                    className="draw-image" 
+                    src={erasersvg}
+                    alt="Eraser Tool"
+                >
+                </img>
+            </label>
+            <label className="label-group">
+                <input 
+                    className="range-select"
+                    id="lineWidth"
+                    type="range"
+                    min="1"
+                    max="25"
+                    defaultValue={1}
+                    onChange={strokeSize}
+                >
+                </input>
+            </label>
+            <label className="label-group">
+                <input 
+                    type="radio" 
+                    name="drawingtool" 
+                    className="drawing-input"
+                    id="square"
+                >
+                </input>
+                <img className="draw-image" src={square}></img>
+            </label>
+            <label className="label-group">
+                <input 
+                    type="radio" 
+                    name="drawingtool" 
+                    className="drawing-input"
+                    id="ellipse"
+                    >
+                </input>
+                <img className="draw-image" src={ellipse}></img>
+            </label>
+            <label className="label-group">
+                <input 
+                    type="radio" 
+                    name="drawingtool" 
+                    className="drawing-input"
+                    id="triangle"
+                    >
+                </input>
+                <img className="draw-image" src={triangle}></img>
+            </label>
+            <label className='label-group'>
+                <input
+                    type='color'
+                    className='color-input'
+                    onInput={colorPicker}
+                    id='color-picker'
+                >
+                </input>
+            </label> 
+            <button 
+                className='btn-decoration'
+                onClick={fillCanvas}
+                >
+                    <img 
+                        src={filltool} 
+                        alt="Fill Tool"
+                        >
+                    </img>
+            </button>
+        </div>
+     </>
+    );
+}
+
+export default Toolbar;
--- src/Contexts/DrawingContext.jsx
+++ src/Contexts/DrawingContext.jsx
@@ -1,4 +1,26 @@
-import { createContext } from "react";
+import { createContext, useRef, useCallback } from "react";
 
 export const DrawingContext = createContext();
+
+export const canvasRef = () => {
+    const drawCanvasRef = useRef(null);
+    const displayCanvasRef = useRef(null);
+
+    const getContext = useCallback(() => {
+        const drawCanvas = drawCanvasRef.current;
+        const context = drawCanvas.getContext('2d');
+        return { drawCanvas, context };
+      }, [drawCanvasRef]);
+    
+      const getContextDisplay = useCallback(() => {
+        const displayCanvas = displayCanvasRef.current;
+        const displayContext = displayCanvas.getContext('2d');
+        return { displayCanvas, displayContext };
+      }, [displayCanvasRef]);
+
+    return { drawCanvasRef , displayCanvasRef, getContext, getContextDisplay }
+}
+
+
+
 
